665de1b91a396bd133474a043b774867527193f3
kemuelkesley <kemuelkesleytkd@outlook.com>
2024-07-13 13:25:47 -0300

    nova fonte adicionada, e novos calculos.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 function calcularTributos() {
-    const valor = parseFloat(document.getElementById('valor').value.replace(',', '.'));
+    let valorStr = document.getElementById('valor').value;
+    // Remover caracteres que não sejam dígitos ou pontos
+    valorStr = valorStr.replace(/\./g, '').replace(',', '.');
 
+    const valor = parseFloat(valorStr);
 
     if (isNaN(valor)) {
         alert('Por favor, insira um valor válido.');
         return;
     }
 
-    
     const pis = valor * 0.0065;
     const cofins = valor * 0.03;
     const ir = valor * 0.015;
@@ -24,8 +26,7 @@ function calcularTributos() {
     document.getElementById('valorRestante').textContent = valorRestante.toFixed(2);
 }
 
-
-function limparCampos(){
+function limparCampos() {
     document.getElementById('valor').value = '';
     document.getElementById('pis').textContent = '0.00';
     document.getElementById('cofins').textContent = '0.00';
@@ -34,19 +35,20 @@ function limparCampos(){
     document.getElementById('totalTributos').textContent = '0.00';
     document.getElementById('valorRestante').textContent = '0.00';
 
-
-    if (document.getElementById('valor').value === ''){
-        document.getElementById('valor').focus();        
+    if (document.getElementById('valor').value === '') {
+        document.getElementById('valor').focus();
     }
 }
 
-
-function pressionarEnter(){
-    valor.addEventListener('keydown', function(event) {
+function pressionarEnter() {
+    document.getElementById('valor').addEventListener('keydown', function(event) {
         if (event.key === 'Enter') {
-          calcularTributos();
-        }       
+            calcularTributos();
+        }
     });
 }
 
-pressionarEnter();   
+$(document).ready(function() {
+    $('#valor').mask('000.000.000.000.000,00', {reverse: true});
+    pressionarEnter();
+});
